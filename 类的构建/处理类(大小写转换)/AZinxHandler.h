5ae03bbee571da3cc95c22e6ef6700e0b250fbc7
God-farmer <59203351+God-farmer@users.noreply.github.com>
2020-03-11 21:10:09 +0800

    两个处理

--- 类的构建/处理类(大小写转换)/AZinxHandler.h
+++ 类的构建/处理类(大小写转换)/AZinxHandler.h
@@ -16,6 +16,8 @@ public:
 	virtual ~AZinxHandler();
 
 	//处理入口方法
+	//这个基类只实现一个方法，这个方法中会调用下两个方法
+	//纯虚函数->所以三个方法都只能在子类中实现
 	void handle(AZinxMsg &_msg);
 
 	//内部处理，由子类实现
@@ -24,5 +26,9 @@ public:
 	//获取下一个处理
 	virtual AZinxHandler* getNextHandler(AZinxMsg &_msg) = 0;
 
+//讲道理我觉得应该是private
+public:
+	//下一个处理指针
+	AZinxHandler *pNextHandler = nullptr;
 };
 
