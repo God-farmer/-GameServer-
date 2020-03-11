a043cb1fda29c4a6f08ac57153170fe7d1976af9
Mr.soul <728673852@qq.com>
2020-03-11 10:48:38 +0800

    TCP通道类

---
+++ 类的构建/TCP通道类/TCPChannel.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+#include "IChannel.h"
+#include <arpa/inet.h>
+#include <sys/types.h>          /* See NOTES */
+#include <sys/socket.h>
+#include <unistd.h>
+
+class TCPChannel :
+	public IChannel
+{
+public:
+	TCPChannel();
+	virtual ~TCPChannel();
+	// 通过 IChannel 继承
+	virtual bool init(void) override;
+	virtual bool fini(void) override;
+	virtual string readFd(string & _data) override;
+	virtual int writeFd(string & _data) override;
+	virtual int getFd() override;
+
+private:
+	int mSockFd = -1;
+	int mConnFd = -1;
+	struct sockaddr_in mAddr;
+};
+
