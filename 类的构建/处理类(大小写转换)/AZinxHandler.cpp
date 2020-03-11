4986a89c66cb17d23abe194fdc228bac541387cb
God-farmer <59203351+God-farmer@users.noreply.github.com>
2020-03-11 21:00:24 +0800

    处理类(大小写转换)

---
+++ 类的构建/处理类(大小写转换)/AZinxHandler.cpp
@@ -0,0 +1,39 @@
+﻿#include "AZinxHandler.h"
+
+//Msg构造函数
+AZinxMsg::AZinxMsg()
+{
+}
+
+AZinxMsg::~AZinxMsg()
+{
+}
+
+//Handler构造函数
+AZinxHandler::AZinxHandler()
+{
+}
+
+AZinxHandler::~AZinxHandler()
+{
+}
+
+//处理入口
+void AZinxHandler::handle(AZinxMsg & _msg)
+{
+	AZinxMsg *pMsg = nullptr;
+	AZinxHandler *pHandler = nullptr;
+
+	//执行内部的操作
+	pMsg = internalHandle(_msg);
+	if (nullptr != pMsg)
+	{
+		//获取下一个处理
+		pHandler = getNextHandler(*pMsg);
+		if (nullptr != pHandler)
+		{
+			pHandler->handle(*pMsg);
+		}
+	}
+
+}
---
+++ 类的构建/处理类(大小写转换)/AZinxHandler.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+//抽象处理消息类
+class AZinxMsg
+{
+public:
+    AZinxMsg(/* args */);
+    virtual ~AZinxMsg();
+};
+
+//抽象处理类
+class AZinxHandler
+{
+public:
+	AZinxHandler();
+	virtual ~AZinxHandler();
+
+	//处理入口方法
+	void handle(AZinxMsg &_msg);
+
+	//内部处理，由子类实现
+	virtual AZinxMsg* internalHandle(AZinxMsg &_msg) = 0;
+
+	//获取下一个处理
+	virtual AZinxHandler* getNextHandler(AZinxMsg &_msg) = 0;
+
+};
+
